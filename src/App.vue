16574b32bf9f9cdf19e004d52db2cf0f09f8fe32
Fiasko99 <snakeice2000@mail.ru>
2022-07-16 13:21:08 +0300

    v-if base use

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,33 @@
   </ul>
   <h1>APP VUE</h1>
   <router-view></router-view>
+  <div>
+    <button @click="changeSelect('local')">Local</button>
+    <button @click="changeSelect('global')">Global</button>
+    <button @click="changeSelect('')">Nothing</button>
+  </div>
   <main>
-    <local-component />
-    <global-component />
+    <local-component v-if="selected == 'local'" />
+    <global-component v-else-if="selected == 'global'" />
+    <h1 v-else>Нет компонента</h1>
   </main>
 </template>
 
 <script>
 import LocalComponent from "@/components/Lacal.vue";
 export default {
+  data() {
+    return {
+      selected: ''
+    }
+  },
   components: {
     LocalComponent
+  },
+  methods: {
+    changeSelect(page) {
+      this.selected = page
+    }
   }
 }
 </script>
