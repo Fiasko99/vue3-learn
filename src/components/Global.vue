43b2a20d07f23e65d14e316f8f161311c2439641
Fiasko99 <snakeice2000@mail.ru>
2022-07-16 13:12:50 +0300

    base v-model

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -1,10 +1,17 @@
 <template>
   <h1>Global component</h1>
+  <input type="text" name="text" id="text" v-model="text">
+  <h3>{{text}}</h3>
 </template>
 
 <script>
 export default {
-  name: 'global-component'
+  name: 'global-component',
+  data() {
+    return {
+      text: 'Первичный текст'
+    }
+  }
 }
 </script>
 
